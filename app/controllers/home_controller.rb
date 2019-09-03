8a782c0ed32c3eff6b4457a7674231a8153f34fa
Chris Hill-Scott <me@quis.cc>
2019-09-03 11:37:07 +0100

    WIP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def index
     return redirect_to super_admin_organisations_path if super_admin?
 
-    redirect_to(current_organisation.ips.empty? ? new_organisation_setup_instructions_path : overview_index_path)
+    redirect_to(new_organisation_setup_instructions_path)
   end
 end
--- app/controllers/overview_controller.rb
+++
@@ -1,7 +0,0 @@
-class OverviewController < ApplicationController
-  def index
-    @ips = current_organisation.ips
-    @locations = current_organisation.locations
-    @team_members = current_organisation.users
-  end
-end
